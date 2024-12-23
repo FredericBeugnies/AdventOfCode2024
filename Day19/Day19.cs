7f2a35dc241b5e03fd84db5fb09865f2d86ed0db
Frédéric Beugnies <frederic.beugnies@gmail.com>
2024-12-23 19:30:10 +0100

    Day 19 complete

--- Day19/Day19.cs
+++ Day19/Day19.cs
@@ -7,36 +7,37 @@ for (int i = 2; i < input.Length; i++)
     desiredDesigns.Add(input[i]);
 }
 
-// part 1
-int nbPossibleDesigns = 0;
+// part 1 & 2 combined
+int nbDesignsPossible = 0;
+long totalNbArrangements = 0;
 foreach (var desiredDesign in desiredDesigns)
 {
-    if (IsPossible(desiredDesign, availableTowels))
-    {
-        ++nbPossibleDesigns;
-    }
+    long nbArrangements = ComputeNbArrangements(desiredDesign, availableTowels);
+    if (nbArrangements > 0)
+        ++nbDesignsPossible;
+    totalNbArrangements += nbArrangements;
 }
-Console.WriteLine($"Part 1: {nbPossibleDesigns}");
+Console.WriteLine($"Part 1: {nbDesignsPossible}");
+Console.WriteLine($"Part 2: {totalNbArrangements}");
 
-bool IsPossible(string design, string[] availableTowels)
+long ComputeNbArrangements(string design, string[] availableTowels)
 {
-    // isPossible[i] = is it possible to make the [i ... end] part of 'design' using the available towels
-    Dictionary<int, bool> isPossible = [];
-    isPossible[design.Length] = true;
+    // nbWays[i] = number of ways it is possible make the [i ... end] part of 'design' using the available towels
+    Dictionary<int, long> nbArrangements = [];
+    nbArrangements[design.Length] = 1;
 
     for (int i = design.Length - 1; i >= 0; i--)
     {
         string subDesign = design.Substring(i);
-        isPossible[i] = false;
+        nbArrangements[i] = 0;
 
         foreach (string t in availableTowels)
         {
-            if (subDesign.StartsWith(t) && isPossible[i + t.Length])
+            if (subDesign.StartsWith(t))
             {
-                isPossible[i] = true;
-                break;
+                nbArrangements[i] += nbArrangements[i + t.Length];
             }
         }
     }
-    return isPossible[0];
+    return nbArrangements[0];
 }
