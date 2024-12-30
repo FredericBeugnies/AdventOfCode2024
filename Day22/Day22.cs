f193eec4a05dd2048ba415628c1c2b362cb0f32c
Frédéric Beugnies <frederic.beugnies@gmail.com>
2024-12-30 11:48:13 +0100

    Day 22 part 1

---
+++ Day22/Day22.cs
@@ -0,0 +1,31 @@
+﻿var input = File.ReadAllLines("input22.txt");
+
+long res = 0;
+foreach (var line in input)
+{
+    long secretNumber = long.Parse(line);
+    for (int k = 0; k < 2000; ++k)
+    {
+        Mix(ref secretNumber, secretNumber * 64);
+        Prune(ref secretNumber);
+
+        Mix(ref secretNumber, secretNumber / 32);
+        Prune(ref secretNumber);
+
+        Mix(ref secretNumber, secretNumber * 2048);
+        Prune(ref secretNumber);
+    }
+    res += secretNumber;
+}
+
+Console.WriteLine(res);
+
+void Mix(ref long a, long b)
+{
+    a ^= b;
+}
+
+void Prune(ref long a)
+{
+    a %= 16777216;
+}
